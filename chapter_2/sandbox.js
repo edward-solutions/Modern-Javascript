7cd6b71e628cc9b08f4164267708e845fd6102c2
edward cabanatan <edward.cabanatan@gmail.com>
2025-11-27 21:23:19 +0800

    Update variables example in sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,1 +1,14 @@
-alert('hello, world')
+let age = 25;
+let year = 2025;
+year = "A";
+
+console.log(age, year);
+
+age = 30;
+
+console.log(age, year);
+
+var score = 25;
+var score = 100;
+
+console.log(score);
